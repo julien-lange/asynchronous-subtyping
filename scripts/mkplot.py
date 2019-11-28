7c747ac513f927f2063a0fba7ac8f4bf3e8ad278
Julien Lange <j.s.lange@kent.ac.uk>
2019-11-28 12:59:53 +0000

    new curve fittings

--- scripts/mkplot.py
+++ scripts/mkplot.py
@@ -18,7 +18,7 @@ mymarkersize=5
 
 
 def fitfunc(x, a, b, c ):
-    return a * np.power(b, x) - c
+    return a * np.power(b, x) + c
 
 def mkPlot(bfile,outpath):
     tab = np.loadtxt(bfile,
@@ -41,51 +41,33 @@ def mkPlot(bfile,outpath):
 
     linx = np.array(tr)
     liny = np.array(tk)
-    lspace = np.linspace(0,max(tr))
-
-
-    popt, pcov = curve_fit(fitfunc, linx, liny, bounds=(0, [2, 7, 5]))
+    linspace = np.linspace(0,max(tr))
+    logspace = np.logspace(0,max(tr))
+    popt, pcov = curve_fit(fitfunc, linx, liny, bounds=(0, [100, 10, 20]))
 
     print("Fitted curve: "+str(popt))
     # print(pcov)
 
-   
-    
 
-    # comps = bfile.split("-")
-    # xmin = comps[3]
-    # xmax = comps[4]
-    # ymin = comps[6]
-    # ymax = comps[7]
-    # pmin = comps[9]
-    # pmax = comps[10]
-
-    preflegend = "Time" # "Time with "
-    memlegend = "Memory"
+    preflegend = "Execution time" # "Time with "
+    memlegend = "Memory usage"
     legend = "" # "NA"
-    # if int(xmin) != (int(xmax)-1):
-    #     legend = "k="+ymin+", n=1..10, m="+pmin
-    # if int(ymin) != (int(ymax)-1):
-    #     legend =  "k=2..100, n="+xmin+", m="+pmin
-    # if int(pmin) != (int(pmax)-1):
-    #     legend = "k="+ymin+", n="+xmin+", m=1..26"
-    
-
-
 
 
     plt.yticks(fontsize=ticksfontsize)
     plt.xticks(rotation=40, ha='right', fontsize=ticksfontsize)
-    
+  
     plt.grid( linestyle='dotted', color='gray')
 
     ax.set_ylabel('Time (seconds)', fontsize=axisfontsize)
 
+    plt.yscale('log')
     
     ax2 = ax.twinx()
+    ax2.set_yscale("log")
 
     
-    ax.plot(lspace, fitfunc(lspace, *popt),color='orange',zorder=10) 
+    ax.plot(linspace, fitfunc(linspace, *popt),color='orange',zorder=10)  #, linestyle='None',marker='.') 
     ax.plot(tr,tk,marker='o',markersize=mymarkersize,linestyle='None',color='blue',zorder=20)
     ax2.plot(tr, tm,marker='.',markersize=mymarkersize,linestyle='None',color='red',zorder=15)
 
@@ -94,15 +76,26 @@ def mkPlot(bfile,outpath):
 
     ax2.legend([memlegend],loc=(0.05,0.68),fontsize=legentfontsize)
     
+    
+    # ax.legend(
+    #     [r'$F(x)\approx%5.5f * %5.4f^x +%5.4f$' % tuple(popt),
+    #      preflegend+legend,memlegend], loc=(0.05,0.8),fontsize=legentfontsize)
 
+
+    
+    (ca,cb,cc) = tuple(popt)
     ax.legend(
-        [r'$F(x)=%5.5f * %5.4f^x -%5.4f$' % tuple(popt),
+        [r'$F(x)\approx%5.5f * %5.4f^x$' % (ca,cb),
          preflegend+legend,memlegend], loc=(0.05,0.8),fontsize=legentfontsize)
 
 
-    plt.yscale('linear')
 
-    ax.set_xlabel( r'Number of branches',fontsize=axisfontsize)
+    
+
+    xlabel = "m"
+    if bfile.find("_A_") > 1:
+        xlabel = "n"
+    ax.set_xlabel( r'Parameter $'+xlabel+'$',fontsize=axisfontsize)
     plt.xscale('linear')
 
 
